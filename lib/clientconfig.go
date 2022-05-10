293dd345650345ee0da4f861a38477810d9e5a61
Liu Ming <extraf@live.cn>
2022-05-10 14:15:40 +0800

    add mspdir for enroll and register functoin

--- lib/clientconfig.go
+++ lib/clientconfig.go
@@ -79,13 +79,13 @@ func (c *ClientConfig) GetCustomizedIP() string {
 // Enroll a client given the server's URL and the client's home directory.
 // The URL may be of the form: http://user:pass@host:port where user and pass
 // are the enrollment ID and secret, respectively.
-func (c *ClientConfig) Enroll(req *api.EnrollmentRequest, home string) (*EnrollmentResponse, error) {
+func (c *ClientConfig) Enroll(req *api.EnrollmentRequest, home, mspDir string) (*EnrollmentResponse, error) {
 	if req.Name == "" {
 		return nil, errors.Errorf(
 			"Missing the enrollment ID and secret;"+
 				" found '%s' with out user and secret", c.URL)
 	}
-	client := &Client{HomeDir: home, Config: c}
+	client := &Client{HomeDir: home, Config: c, MSPDir: mspDir}
 	return client.Enroll(req)
 }
 
@@ -161,9 +161,9 @@ func (cfg *ClientConfig) ProcessAttributeStrings(cfgAttrReqs []string) error {
 
 // Register registers a new identity
 // @param req The registration request
-func (c *ClientConfig) Register(home string) (rr *RegistrationResponse, err error) {
+func (c *ClientConfig) Register(home, mspDir string) (rr *RegistrationResponse, err error) {
 
-	client := &Client{HomeDir: home, Config: c}
+	client := &Client{HomeDir: home, Config: c, MSPDir: mspDir}
 	client.HomeDir = home
 
 	id, err := client.LoadMyIdentity()
